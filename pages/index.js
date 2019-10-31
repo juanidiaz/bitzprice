8c6f04ac5777a9dc54605c4e85303c58de2cb89e
Juan Diaz <jdi@idiaz.ca>
2019-10-30 23:08:26 -0400

    Implemented next-less plugin

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout';
 import Prices from '../components/Prices';
+import '../style.less'
 
 const Index = (props) => (
   <Layout>
     <div>
       <h1>Welcome to BitzPrice</h1>
       <p>Check current Bitcoin rate</p>
+      <p className='example'>HELLO WORLD</p>
       <Prices bpi={props.bpi} />
     </div>
   </Layout>
